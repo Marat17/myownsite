8d56922a95efca6faa400fa4908a8fcf929f77fb
Marat17 <muhutdinov@hotmail.com>
2018-07-28 14:26:01 +0300

    *admin and model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from blog.models import BlogPost
 
+admin.site.register(BlogPost)
+
 class BlogPostAdmin(admin.ModelAdmin):
 	fieldsets = [
 	('The post', {'fields' : ['post_text']}),
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
 
 class BlogPost(models.Model):
 	post_text = models.CharField(max_length=140)
 	post_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.post_text
 
 # Create your models here.
