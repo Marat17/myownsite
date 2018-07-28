f0b93637ca3e2568df44958f6dd8e02f2c98c0d6
Marat17 <muhutdinov@hotmail.com>
2018-07-28 13:35:19 +0300

    created a model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class BlogPost(models.Model):
+	post_text = models.CharField(max_length=140)
+	post_date = models.DateTimeField('date published')
+
 # Create your models here.
