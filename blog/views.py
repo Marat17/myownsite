27c429be51b29f55562ffc5c7890781535c82ad3
Marat17 <muhutdinov@hotmail.com>
2018-07-28 14:41:36 +0300

    new view

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse('blog index page works!')
 
+def about(request):
+	return HttpResponse("here you'll see some info about this blog")
+
 # Create your views here.
