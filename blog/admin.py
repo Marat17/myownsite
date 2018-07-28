60e5e7b1f620c061e9fec4f149e3276cd79ae163
Marat17 <muhutdinov@hotmail.com>
2018-07-28 13:50:09 +0300

    superuser created and admin changed, but no, I can't add data to the database when there are no posts yet

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import BlogPost
+
+class BlogPostAdmin(admin.ModelAdmin):
+	fieldsets = [
+	('The post', {'fields' : ['post_text']}),
+	('Date of publication', {'fields': ['post_date']}),
+	]
 
 # Register your models here.
