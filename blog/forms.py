125e4b662c6aa98c963bf4e0820eb9342e35905d
Marat17 <muhutdinov@hotmail.com>
2018-07-31 22:29:24 +0300

    Now it is completely finished. Font-size changed

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class CreateNewPost(forms.ModelForm):
-    post_text = forms.CharField(max_length=140, help_text='Please type your new post here.', widget=forms.Textarea)
+    post_text = forms.CharField(max_length=140, help_text='Please type your new post here:', widget=forms.Textarea)
     
     class Meta:
         model = BlogPost
