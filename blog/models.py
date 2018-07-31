fef58750bfdf706adc7a148160d1e7fa03dfaf6a
Marat17 <muhutdinov@hotmail.com>
2018-07-31 20:04:17 +0300

    done

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class BlogPost(models.Model):
     post_text = models.CharField(max_length=140)
-    post_date = models.DateTimeField('date published', default=timezone.now())
+    post_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.post_text
