043f17ce9fafd69866c95f35016614f01e1e6073
Marat17 <muhutdinov@hotmail.com>
2018-07-31 19:17:36 +0300

    a post deleted

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
-import datetime
 from django.db import models
 from django.utils import timezone
 
 
 class BlogPost(models.Model):
-	post_text = models.CharField(max_length=140)
-	post_date = models.DateTimeField('date published', default=timezone.now())
-	def __str__(self):
-		return self.post_text
+    post_text = models.CharField(max_length=140)
+    post_date = models.DateTimeField('date published', default=timezone.now())
+
+    def __str__(self):
+        return self.post_text
 
 # Create your models here.
